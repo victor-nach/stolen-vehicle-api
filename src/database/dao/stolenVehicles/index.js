bd9d0f717abc154e0b7f1e4608e0cd3bf6028ff7
victor <viheanaco@gmail.com>
2019-09-28 22:01:04 +0100

    ft: adjust dao to throw error on empty data return

--- src/database/dao/stolenVehicles/index.js
+++ src/database/dao/stolenVehicles/index.js
@@ -1,14 +1,26 @@
 import db from '../../config';
 
 class StolenVehicles {
+  /**
+  * @name getAllStolenVehicles
+  * @returns database rows
+  * @description searches for a all the stolen vehicles in the database
+*/
   static async getAllStolenVehicles() {
     const { rows } = await db.query('SELECT * FROM vehicles');
     return rows;
   }
 
+  /**
+  * @name getOneStolenVehicle
+  * @param { Number } status
+  * @returns database row
+  * @description searches for a single stolen vehicle by chassis number or id
+*/
   static async getOneStolenVehicle(id) {
-    const { rows } = await db.query('SELECT * FROM vehicles WHERE id = $1', [id]);
-    return rows;
+    const { rows } = await db.query('SELECT * FROM vehicles WHERE "vehicleChassisNumber" = $1', [id]);
+    if (!rows[0]) throw new Error('no_match');
+    return rows[0];
   }
 }
 
