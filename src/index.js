f5c8d257e81b754ad85b12d281bbf4782fbf1de4
victor <viheanaco@gmail.com>
2019-09-28 15:55:25 +0100

    ch: refactor exports to named exports #2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import express from 'express';
+import { responseShort } from './utils/responseMsg';
 
 const app = express();
 const PORT = 3000 || process.env.PORT;
 
 app.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'welcome',
-  });
+  responseShort(res, 200, 'welcome');
 });
 
 app.listen(PORT);
--- src/utils/responseMsg.js
+++ src/utils/responseMsg.js
@@ -1,49 +1,31 @@
-class ResponseMsg {
-  /**
-       * @static responseErr
-       * @param { Object } res
-       * @param { Number } status
-       * @param { Object } message
-       * @returns respsonse body
-       * @description ddefines the standard response format in the case of an error
-       * @memberof ResponseMsg
-       */
-  static responseErr(res, status, message) {
-    return res.status(status).json({
-      status: 'error',
-      error: message,
-    });
-  }
+/**
+  * @param { Object } res
+  * @param { Number } status
+  * @param { Object } message
+  * @returns respsonse body
+  * @description ddefines the standard response format in the case of an error
+*/
+export const responseErr = (res, status, message) => res.status(status).json({
+  status: 'error',
+  error: message,
+});
 
-  /**
-       * @static response
-       * @param { Object } res
-       * @param { Number } status
-       * @param { Object } message
-       * @returns respsonse body
-       * @description ddefines the standard response format when a data object is to be returned
-       * @memberof ResponseMsg
-       */
-  static response(res, status, data) {
-    return res.status(status).json({
-      status: 'success',
-      data,
-    });
-  }
+/**
+  * @param { Object } res
+  * @param { Number } status
+  * @param { Object } message
+  * @returns respsonse body
+  * @description ddefines the standard response format when a data object is to be returned
+*/
+export const response = (res, status, data) => res.status(status).json({
+  status: 'success',
+  data,
+});
 
-  /**
-       *
-       *
-       * @static
-       * @param {*} res
-       * @param {*} status
-       * @param {*} message
-       * @returns
-       * @memberof ResponseMsg
-       */
-  static responseShort(res, status, message) {
-    return res.status(status).json({ status: 'success', message });
-  }
-}
-
-export default ResponseMsg;
+/**
+  * @param { object } res
+  * @param { number } status
+  * @param { string } message
+  * @returns json response message
+*/
+export const responseShort = (res, status, message) => res.status(status).json({ status: 'success', message });
