31683871c8ea7a103db3e42d7cacaec44deb05c0
victor-nach <46886694+victor-nach@users.noreply.github.com>
2019-10-01 23:53:09 +0100

    Update index.js

--- src/controllers/vehicles/index.js
+++ src/controllers/vehicles/index.js
@@ -33,6 +33,6 @@ export const createStolenVehicles = async (req, res) => {
     const data = await StolenVehicles.createStolenVehicle(req.body);
     return response(res, 200, data);
   } catch (error) {
-    return responseErr(res, 500, 'Service Temporarily Down, please try again later');
+    return responseErr(res, 500, error.message);
   }
 };
